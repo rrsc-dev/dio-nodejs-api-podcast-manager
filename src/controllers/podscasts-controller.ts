ab6bb5d888caea790ba90f49b14f481f86e43cc5
Raphael R <reis.rrsc@outlook.com>
2025-12-29 08:01:16 -0300

    acionamento do service e comentários

--- src/controllers/podscasts-controller.ts
+++ src/controllers/podscasts-controller.ts
@@ -1,11 +1,16 @@
 import { IncomingMessage, ServerResponse } from 'http';
 
+import {serviceListEpisodes } from '../services/list-episodes-service';
+
 export const getListEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
+
+    const content = await serviceListEpisodes();
+
     res.writeHead(200, {
         'content-type': 'application/json'
     }); // Protocolo http formado por cabeçalho e corpo, onde o cabeçalho traz metadados sobre a requisição ou resposta, como tipo de conteúdo, status, etc.
 
-    res.end(JSON.stringify({
-        name: 'test',
-    }));
+    res.end(JSON.stringify(content));
 }
+
+// import e export só são usados com o type module no package.json, mas com o typescript não é necessário usar o type module, pois o tsc já faz a transpilação para commonjs automaticamente

66a498573eee602fc78ac3d0dfb6b7fe7afd0fe1
Raphael R <reis.rrsc@outlook.com>
2025-12-29 14:30:43 -0300

    alteracao no repositorio para filtro de podcast

--- src/repositories/podcasts-repository.ts
+++ src/repositories/podcasts-repository.ts
@@ -1,13 +1,17 @@
 import fs from "fs";
 import path from "path";
-import { Podcast } from "../models/podcast-model";
+import { PodcastModel } from "../models/podcast-model";
 
 const pathData = path.join(__dirname, "../repositories/podcasts.json"); // dirname não funciona com o type module do package.json, uma das soluções é deixar o módulo dinâmico, retirando o type module do package.json
 
-export const repoPodcast = async (): Promise<Podcast[]> => {
+export const repoPodcast = async (podcastName?: string): Promise<PodcastModel[]> => {
+    // Função com parâmetro opcional
     const rawData = fs.readFileSync(pathData, "utf-8");
-    const jsonFile = JSON.parse(rawData);
+    let jsonFile = JSON.parse(rawData);
+
+    if (podcastName) {
+        jsonFile = jsonFile.filter((podcast: PodcastModel) => podcast.podcastName === podcastName);
+    }
 
     return jsonFile;
 }
-

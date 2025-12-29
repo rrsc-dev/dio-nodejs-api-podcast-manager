ed22108f276a7ba230b7ab0f36426efa07499875
rrsc-dev <reis.rrsc@outlook.com>
2025-12-28 22:33:53 -0300

    test controller

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log("ok")
+import * as http from 'http';
+import { getListEpisodes } from './controllers/podscasts-controller';
+
+const port = process.env.PORT || 3333;
+
+const server = http.createServer( async (req: http.IncomingMessage, res: http.ServerResponse) => {
+    if(req.method === 'GET') {
+        await getListEpisodes(req, res);
+    }
+});
+
+server.listen(port, () => {
+    console.log(`Servidor iniciado na porta ${port}`);
+})

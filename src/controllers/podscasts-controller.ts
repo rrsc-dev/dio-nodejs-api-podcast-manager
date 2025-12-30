3ac170ce4d3d0e3da86a38cdc80810e850854d1d
rrsc-dev <reis.rrsc@outlook.com>
2025-12-29 23:07:34 -0300

    rotas e queryString

--- src/controllers/podscasts-controller.ts
+++ src/controllers/podscasts-controller.ts
@@ -1,6 +1,7 @@
 import { IncomingMessage, ServerResponse } from 'http';
 
 import {serviceListEpisodes } from '../services/list-episodes-service';
+import { serviceFilterEpisodes } from '../services/filter-episodes-service';
 
 export const getListEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
 
@@ -14,3 +15,15 @@ export const getListEpisodes = async (req: IncomingMessage, res: ServerResponse)
 }
 
 // import e export só são usados com o type module no package.json, mas com o typescript não é necessário usar o type module, pois o tsc já faz a transpilação para commonjs automaticamente
+
+export const getFilterEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
+    const queryString = req.url?.split("?p=")[1] ?? '';
+
+    const content = await serviceFilterEpisodes(queryString);
+
+    res.writeHead(200, {
+        'content-type': 'application/json'
+    });
+
+    res.end(JSON.stringify(content));
+}
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import * as http from 'http';
-import { getListEpisodes } from './controllers/podscasts-controller';
+import { getFilterEpisodes, getListEpisodes } from './controllers/podscasts-controller';
 
 const port = process.env.PORT || 3333;
 
 const server = http.createServer( async (req: http.IncomingMessage, res: http.ServerResponse) => {
-    if(req.method === 'GET') {
+
+    // query string
+    const [baseUrl, queryString] = req.url?.split("?") ?? [];
+
+    // Listas podcasts
+    if(req.method === 'GET' && req.url === '/api/list') {
         await getListEpisodes(req, res);
+    }
+
+    if (req.method === 'GET' && req.url === '/api/episode') {
+        await getFilterEpisodes(req, res);
     }
 });
 

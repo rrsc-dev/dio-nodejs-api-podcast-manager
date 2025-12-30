6ecd70aece8311c6c46bed21784b35cc37512cd9
Raphael R <reis.rrsc@outlook.com>
2025-12-30 07:08:52 -0300

    inicio refatoração

--- src/controllers/podscasts-controller.ts
+++ src/controllers/podscasts-controller.ts
@@ -17,9 +17,9 @@ export const getListEpisodes = async (req: IncomingMessage, res: ServerResponse)
 // import e export só são usados com o type module no package.json, mas com o typescript não é necessário usar o type module, pois o tsc já faz a transpilação para commonjs automaticamente
 
 export const getFilterEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
-    const queryString = req.url?.split("?p=")[1] ?? '';
+    // const queryString = req.url?.split("?p=")[1] ?? ''; // removido porque o controller não deve ter lógica de negócio
 
-    const content = await serviceFilterEpisodes(queryString);
+    const content = await serviceFilterEpisodes(req.url);
 
     res.writeHead(200, {
         'content-type': 'application/json'

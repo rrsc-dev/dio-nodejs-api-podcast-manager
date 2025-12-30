357086592f5457b3bbfbbaca3e6d8da8389607bd
Raphael R <reis.rrsc@outlook.com>
2025-12-30 07:14:12 -0300

    refatoração rotas

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import * as http from 'http';
 import { getFilterEpisodes, getListEpisodes } from './controllers/podscasts-controller';
+import { Routes } from './routes/routes';
 
 const port = process.env.PORT || 3333;
 
@@ -9,11 +10,11 @@ const server = http.createServer( async (req: http.IncomingMessage, res: http.Se
     const [baseUrl, queryString] = req.url?.split("?") ?? [];
 
     // Listas podcasts
-    if(req.method === 'GET' && req.url === '/api/list') {
+    if(req.method === 'GET' && req.url === Routes.LIST) {
         await getListEpisodes(req, res);
     }
 
-    if (req.method === 'GET' && req.url === '/api/episode') {
+    if (req.method === 'GET' && req.url === Routes.EPISODE) {
         await getFilterEpisodes(req, res);
     }
 });

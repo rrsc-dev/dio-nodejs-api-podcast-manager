30048fadb7b01beab093fb8bf497a9a932ba9046
Raphael R <reis.rrsc@outlook.com>
2026-01-05 07:05:13 -0300

    Validação de status no list episode service

--- src/controllers/podscasts-controller.ts
+++ src/controllers/podscasts-controller.ts
@@ -8,13 +8,14 @@ import { FilterPodcastModel } from '../models/filter-podcast-model';
 
 export const getListEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
 
-    const content = await serviceListEpisodes();
+    const content: FilterPodcastModel = await serviceListEpisodes();
 
-    res.writeHead(StatusCode.OK, {
+    res.writeHead(content.statusCode, {
         'content-type': 'application/json'
     }); // Protocolo http formado por cabeçalho e corpo, onde o cabeçalho traz metadados sobre a requisição ou resposta, como tipo de conteúdo, status, etc.
-
-    res.end(JSON.stringify(content));
+    
+    res.write(JSON.stringify(content.body));
+    res.end(); // Usado como um ponto final
 }
 
 // import e export só são usados com o type module no package.json, mas com o typescript não é necessário usar o type module, pois o tsc já faz a transpilação para commonjs automaticamente
@@ -28,5 +29,6 @@ export const getFilterEpisodes = async (req: IncomingMessage, res: ServerRespons
         'content-type': ContentType.JSON
     });
 
-    res.end(JSON.stringify(content.body));
+    res.write(JSON.stringify(content.body));
+    res.end();
 }
--- src/services/list-episodes-service.ts
+++ src/services/list-episodes-service.ts
@@ -1,8 +1,33 @@
 import { repoPodcast } from "../repositories/podcasts-repository";
+import { FilterPodcastModel } from "../models/filter-podcast-model";
+
+import { StatusCode } from "../utils/status-code";
 
 
-export const serviceListEpisodes = async ( ) => {
+
+export const serviceListEpisodes = async (): Promise<FilterPodcastModel> => {
+
+    // Definindo o formato de resposta
+    let responseFormat: FilterPodcastModel = {
+        statusCode: 0,
+        body: [],
+    };
+
     const data = await repoPodcast();
 
-    return data;
+    // Validação
+    // if (data.length !== 0) {
+    //     responseFormat.statusCode = StatusCode.OK;
+    // } else {
+    //     responseFormat.statusCode = StatusCode.NO_CONTENT;
+    // }
+
+    // responseFormat.body = data;
+
+    responseFormat = {
+        statusCode: data.length !== 0 ? StatusCode.OK : StatusCode.NO_CONTENT,
+        body: data,
+    }
+
+    return responseFormat;
 }
